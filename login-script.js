75657d2c45534b18d79b2f3a1e6e299d202710af
Luis Guilherme <luisguilhermeti@gmail.com>
2023-08-21 19:45:47 -0300

    Singleton funciona no index!!!

--- login-script.js
+++ login-script.js
@@ -6,6 +6,7 @@ const formOpenBtn = document.querySelector("#form-open"),
   loginBtn = document.querySelector("#login"),
   pwShowHide = document.querySelectorAll(".pw_hide");
   entrar = document.querySelector("#entrar");
+  cadastrar = document.querySelector("#cadastrar");
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
@@ -23,8 +24,6 @@ pwShowHide.forEach((icon) => {
   });
 });
 
-formContainer.classList.add("active");
-
 signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.add("active");
@@ -36,8 +35,12 @@ loginBtn.addEventListener("click", (e) => {
 
 entrar.addEventListener("click",(e)=>{
   //passar a sessão para a página de calendário
-  window.open("calendar.html?loggedAs=luisguilherme", "_self");
-})
+  // window.open("calendar.html?loggedAs=luisguilherme", "_self");
+  login();
+});
+cadastrar.addEventListener("click",(e)=>{
+  cadastro();
+});
 
 const Singleton = (function(){
   function Usuario({nome, senha}) {
@@ -59,3 +62,37 @@ const Singleton = (function(){
     }
   } 
 })();
+
+function login(){
+  if(localStorage.getItem("loggedAs")){
+    alert("Você já está logado como "+localStorage.getItem("loggedAs")+"!");
+    return;
+  }
+  var nome = document.getElementById("user").value;
+  var senha = document.getElementById("pwd").value;
+  if(localStorage.getItem(nome)&&localStorage.getItem(nome+senha)){
+    alert("Bem vindo "+nome+"!");
+    usuario = Singleton.getUsuario();
+    localStorage.setItem("loggedAs",nome);
+    usuario.nome = nome;
+  }else{
+    alert("Usuário ou senha incorretos!");
+  }
+}
+
+function cadastro(){
+  var nome = document.getElementById("novoUsername").value;
+  var senha1 = document.getElementById("pwd1").value;
+  var senha2 = document.getElementById("pwd2").value;
+  if(localStorage.getItem(nome)){
+    alert("Usuário já cadastrado!");
+    return;
+  }
+  if(senha1!=senha2){
+    alert("As senhas devem ser iguais!");
+    return;
+  }
+  localStorage.setItem(nome,nome);
+  localStorage.setItem(nome+senha1,senha1);
+  alert("Usuário cadastrado com sucesso!");
+}
